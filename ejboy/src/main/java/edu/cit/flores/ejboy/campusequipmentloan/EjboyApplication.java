81167e4c647f466ae8c130dee718004d84164f5c
E.J Boy Flores <ejboyflores@gmail.com>
2025-09-25 15:52:01 +0800

    Initial Spring Boot setup

---
+++ ejboy/src/main/java/edu/cit/flores/ejboy/campusequipmentloan/EjboyApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.flores.ejboy.campusequipmentloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjboyApplication.class, args);
+	}
+
+}
---
+++ ejboy/src/test/java/edu/cit/flores/ejboy/campusequipmentloan/EjboyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.flores.ejboy.campusequipmentloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
